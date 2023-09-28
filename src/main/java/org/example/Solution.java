ca9e06d7a6e5bc1114ea28e3edf49ef6a2d5b544
Aliaksei <116200773+AliakseiBaryliuk@users.noreply.github.com>
2023-09-28 09:23:53 +0300

    Solution

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,42 @@ package org.example;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        //write your code here
+    Arrays.sort(strs, Comparator.comparing(String::length));
+
+        if(strs.length == 0) {
+
+            return "";
+
+        }
+
+        var firstStrLength = strs[0].length();
+
+        for (int i = 1; i < strs.length; i++) {
+
+            strs[i] = strs[i].substring(0, firstStrLength);
+
+        }
+
+ 
+
+        while (!strs[0].isEmpty()) {
+
+            if (Arrays.stream(strs).skip(1).allMatch(s -> strs[0].equals(s))) {
+
+                return strs[0];
+
+            }
+
+            firstStrLength--;
+
+            for (int i = 0; i < strs.length; i++) {
+
+                strs[i] = strs[i].substring(0, firstStrLength);
+
+            }
+
+        }
+
         return "";
     }
 }
